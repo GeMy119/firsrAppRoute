1eba35313c1979a8c0479521b984421e64a7850c
GeMy119 <mahmoud.gamal1191998@gmail.com>
2026-07-23 15:40:49 +0300

    first app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./components/home/home.component').then(
+                (m) => m.HomeComponent
+            ),
+    },
+    {
+        path: 'contact',
+        loadComponent: () =>
+            import('./components/contact/contact.component').then(
+                (m) => m.ContactComponent
+            ),
+    },
+    {
+        path: 'about',
+        loadComponent: () =>
+            import('./components/about/about.component').then(
+                (m) => m.AboutComponent
+            ),
+    },
+];
---
+++ src/app/product-list.ts
@@ -0,0 +1,11 @@
+export interface ProductList {
+    id: number,
+    title: string,
+    description: string,
+    imageCover: string,
+    price: number,
+    quantity: number,
+    images: string[],
+    onSale: boolean
+
+}
